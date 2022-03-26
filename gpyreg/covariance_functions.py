79a385bf51ba284e490c31188ddc1428c4bd3483
Gurjeet Singh Sangra <gurjeet.singh@studenti.univr.it>
2022-03-26 16:46:56 +0200

    Covariance Rational Quadratic ARD (#9)
    * feat: new covariance function, rational quadratic-ARD (RQ-ARD)
    
    * feat: new covariance function, rational quadratic-ARD (RQ-ARD)
    
    * feat: removed comments
    
    * feat: introduced an abstract class for kernel classes
    
    * feat: added gradient tests and checks via finite difference
    
    * feat: added the documentation to the test function
    
    * feat: stochastic gradient covariance test
    
    Co-authored-by: GurjeetSinghSangra <sardargurjeet95@gmail.com>

--- gpyreg/covariance_functions.py
+++ gpyreg/covariance_functions.py
@@ -3,15 +3,55 @@
 import numpy as np
 from scipy.spatial.distance import pdist, cdist, squareform
 
+from abc import ABC, abstractclassmethod
 
-class SquaredExponential:
-    """Squared exponential kernel."""
+class AbstractKernel(ABC):
 
-    def __init__(self):
+    @abstractclassmethod
+    def compute(self, hyp: np.ndarray, X: np.ndarray, X_star: np.ndarray = None,
+        compute_diag: bool = False,
+        compute_grad: bool = False,
+    ):
+        """
+            Compute the covariance matrix for given training points
+            and test points.
+
+            Parameters
+            ----------
+            hyp : ndarray, shape (cov_N,)
+                A 1D array of hyperparameters, where ``cov_N`` is
+                the number of hyperparameters.
+            X : ndarray, shape (N, D)
+                A 2D array where each row is a training point.
+            X_star : ndarray, shape (M, D), optional
+                A 2D array where each row is a test point. If this is not
+                given, the self-covariance matrix is being computed.
+            compute_diag : bool, defaults to False
+                Whether to only compute the diagonal of the self-covariance
+                matrix.
+            compute_grad : bool, defaults to False
+                Whether to compute the gradient with respect to the
+                hyperparameters.
+
+            Returns
+            -------
+            K : ndarray
+                The covariance matrix which is by default of shape ``(N, N)``. If
+                ``compute_diag = True`` the shape is ``(N,)``.
+            dK : ndarray, shape (N, N, cov_N), optional
+                The gradient of the covariance matrix with respect to the
+                hyperparameters.
+
+            Raises
+            ------
+            ValueError
+                Raised when `hyp` has not the expected number of hyperparameters.
+            ValueError
+                Raised when `hyp` is not an 1D array but of higher dimension.
+            """
         pass
 
-    @staticmethod
-    def hyperparameter_count(D: int):
+    def hyperparameter_count(self, D: int):
         """
         Return the number of hyperparameters this covariance function has.
 
@@ -27,8 +67,7 @@ class SquaredExponential:
         """
         return D + 1
 
-    @staticmethod
-    def hyperparameter_info(D: int):
+    def hyperparameter_info(self, D: int):
         """
         Return information on the names of hyperparameters for setting
         them in other parts of the program.
@@ -48,7 +87,7 @@ class SquaredExponential:
             ("covariance_log_lengthscale", D),
             ("covariance_log_outputscale", 1),
         ]
-
+    
     def get_bounds_info(self, X: np.ndarray, y: np.ndarray):
         """
         Return information on the lower, upper, plausible lower
@@ -83,51 +122,19 @@ class SquaredExponential:
         cov_N = self.hyperparameter_count(X.shape[1])
         return _bounds_info_helper(cov_N, X, y)
 
+
+class SquaredExponential(AbstractKernel):
+    """Squared exponential kernel."""
+
+    # Overriding abstract method
     def compute(
         self,
         hyp: np.ndarray,
         X: np.ndarray,
         X_star: np.ndarray = None,
         compute_diag: bool = False,
-        compute_grad: bool = False,
-    ):
-        """
-        Compute the covariance matrix for given training points
-        and test points.
+        compute_grad: bool = False,):
 
-        Parameters
-        ----------
-        hyp : ndarray, shape (cov_N,)
-            A 1D array of hyperparameters, where ``cov_N`` is
-            the number of hyperparameters.
-        X : ndarray, shape (N, D)
-            A 2D array where each row is a training point.
-        X_star : ndarray, shape (M, D), optional
-            A 2D array where each row is a test point. If this is not
-            given, the self-covariance matrix is being computed.
-        compute_diag : bool, defaults to False
-            Whether to only compute the diagonal of the self-covariance
-            matrix.
-        compute_grad : bool, defaults to False
-            Whether to compute the gradient with respect to the
-            hyperparameters.
-
-        Returns
-        -------
-        K : ndarray
-            The covariance matrix which is by default of shape ``(N, N)``. If
-            ``compute_diag = True`` the shape is ``(N,)``.
-        dK : ndarray, shape (N, N, cov_N), optional
-            The gradient of the covariance matrix with respect to the
-            hyperparameters.
-
-        Raises
-        ------
-        ValueError
-            Raised when `hyp` has not the expected number of hyperparameters.
-        ValueError
-            Raised when `hyp` is not an 1D array but of higher dimension.
-        """
         N, D = X.shape
         cov_N = self.hyperparameter_count(D)
 
@@ -169,7 +176,7 @@ class SquaredExponential:
         return K
 
 
-class Matern:
+class Matern(AbstractKernel):
     """
     Matern kernel.
 
@@ -200,124 +207,15 @@ class Matern:
             self.f = lambda t: 1 + t * (1 + t / 3)
             self.df = lambda t: (1 + t) / 3
 
-    @staticmethod
-    def hyperparameter_count(D: int):
-        """
-        Return the number of hyperparameters this covariance function has.
-
-        Parameters
-        ----------
-        D : int
-            The dimensionality of the kernel.
-
-        Returns
-        -------
-        count : int
-            The number of hyperparameters.
-        """
-        return D + 1
-
-    @staticmethod
-    def hyperparameter_info(D: int):
-        """
-        Return information on the names of hyperparameters for setting
-        them in other parts of the program.
-
-        Parameters
-        ----------
-        D : int
-            The dimensionality of the kernel.
-
-        Returns
-        -------
-        hyper_info : array_like
-            A list of tuples of hyperparameter names and their number,
-            in the order they are in the hyperparameter array.
-        """
-        return [
-            ("covariance_log_lengthscale", D),
-            ("covariance_log_outputscale", 1),
-        ]
-
-    def get_bounds_info(self, X: np.ndarray, y: np.ndarray):
-        """
-        Return information on the lower, upper, plausible lower
-        and plausible upper bounds of the hyperparameters of this
-        covariance function.
-
-        Parameters
-        ----------
-        X : ndarray, shape (N, D)
-            A 2D array where each row is a test point.
-        y : ndarray, shape (N, 1)
-            A 2D array where each row is a test target.
-
-        Returns
-        -------
-        cov_bound_info: dict
-            A dictionary containing the bound info with the following elements:
-
-            **LB** : np.ndarray, shape (cov_N, 1)
-                    The lower bounds of the hyperparameters.
-            **UB** : np.ndarray, shape (cov_N, 1)
-                    The upper bounds of the hyperparameters.
-            **PLB** : np.ndarray, shape (cov_N, 1)
-                    The plausible lower bounds of the hyperparameters.
-            **PUB** : np.ndarray, shape (cov_N, 1)
-                    The plausible upper bounds of the hyperparameters.
-            **x0** : np.ndarray, shape (cov_N, 1)
-                    The plausible starting point.
-
-            where ``cov_N`` is the number of hyperparameters.
-        """
-        cov_N = self.hyperparameter_count(X.shape[1])
-        return _bounds_info_helper(cov_N, X, y)
-
+    # Overriding abstract method
     def compute(
         self,
         hyp: np.ndarray,
         X: np.ndarray,
         X_star: np.ndarray = None,
         compute_diag: bool = False,
-        compute_grad: bool = False,
-    ):
-        """
-        Compute the covariance matrix for given training points
-        and test points.
-
-        Parameters
-        ----------
-        hyp : ndarray, shape (cov_N,)
-            A 1D array of hyperparameters, where ``cov_N`` is
-            the number of hyperparameters.
-        X : ndarray, shape (N, d)
-            A 2D array where each row is a training point.
-        X_star : ndarray, shape (M, d), optional
-            A 2D array where each row is a test point. If this is not
-            given, the self-covariance matrix is being computed.
-        compute_diag : bool, defaults to False
-            Whether to only compute the diagonal of the self-covariance
-            matrix.
-        compute_grad : bool, defaults to False
-            Whether to compute the gradient with respect to the
-            hyperparameters.
-
-        Returns
-        -------
-        K : ndarray
-            The covariance matrix, which is by default of shape ``(N, N)``. If
-            ``compute_diag = True`` the shape is ``(N,)``.
-        dK : ndarray, shape (N, N, cov_N), optional
-            The gradient of the covariance matrix with respect to the
-            hyperparameters.
-
-        Raises
-        ------
-        ValueError
-            Raised when `hyp` has not the expected number of hyperparameters.
-        ValueError
-            Raised when `hyp` is not an 1D array but of higher dimension.
-        """
+        compute_grad: bool = False,):
+        
         N, D = X.shape
         cov_N = self.hyperparameter_count(D)
 
@@ -370,6 +268,80 @@ class Matern:
             return K, dK
 
         return K
+
+
+class RationalQuadraticARD(AbstractKernel):
+    """Rational Quadratic ARD kernel"""
+
+    def hyperparameter_count(self, D: int):
+        return D + 2
+    
+    def hyperparameter_info(self, D: int):
+        return [
+            ("covariance_log_lengthscale", D),
+            ("covariance_log_outputscale", 1),
+            ("covariance_log_shape", 1)]
+
+    def compute(
+        self,
+        hyp: np.ndarray,
+        X: np.ndarray,
+        X_star: np.ndarray = None,
+        compute_diag: bool = False,
+        compute_grad: bool = False,
+    ):
+        
+        N, D = X.shape
+        cov_N = self.hyperparameter_count(D)
+
+        if hyp.size != cov_N:
+            raise ValueError(
+                "Expected %d covariance function hyperparameters, %d "
+                "passed instead." % (cov_N, hyp.size)
+            )
+        if hyp.ndim != 1:
+            raise ValueError(
+                "Covariance function output is available only for "
+                "one-sample hyperparameter inputs."
+            )
+
+        ell = np.exp(hyp[0:D])
+        sf2 = np.exp(2 * hyp[D])
+        alpha = np.exp(hyp[D+1])
+
+        if X_star is None:
+            if compute_diag:
+                tmp = np.zeros((N, 1))
+            else:
+                tmp = squareform(pdist(X @ np.diag(1. / ell), "sqeuclidean"))
+        else:
+            a = X @ np.diag(1. / ell)
+            b = X_star @ np.diag(1. / ell)
+            tmp = cdist(a, b, "sqeuclidean")
+
+        M = (1 + 0.5 * tmp/alpha) 
+        K = sf2 * M **(-alpha)
+
+        if compute_grad:
+            dK = np.zeros((N, N, cov_N))
+
+            # Gradient respect of lenght scale.
+            for i in range(0, D):
+                Ki = squareform(pdist(np.reshape(1. / ell[i] * X[:, i], (-1, 1)),
+                        "sqeuclidean",))
+                with np.errstate(all="ignore"):
+                    dK[:, :, i] = sf2 * M**(-alpha-1) * Ki
+
+            # Gradient of cov output scale.
+            dK[:, :, D] = 2 * K
+
+            # Gradient respect of alpha.
+            dK[:, :, D+1] = K * (0.5 * tmp/M - alpha * np.log(M))
+
+            return K, dK
+
+        return K
+
 
 
 def _bounds_info_helper(cov_N, X, y):
--- tests/test_covariance_functions.py
+++ tests/test_covariance_functions.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pytest
-from gpyreg.covariance_functions import Matern, SquaredExponential
-
+from gpyreg.covariance_functions import AbstractKernel, Matern, SquaredExponential, RationalQuadraticARD
 
 def test_squared_exponential_compute_sanity_checks():
     squared_expontential = SquaredExponential()
@@ -24,6 +23,16 @@ def test_squared_exponential_compute_sanity_checks():
         in execinfo.value.args[0]
     )
 
+def test_sqr_exp_kernel_gradient():
+    sqr_exp = SquaredExponential()
+    D = 3
+    N = 20
+    diag_cov = np.eye(N)*(0.2)
+    X = (np.random.multivariate_normal(np.zeros(N), diag_cov, D)).T
+    hyp_D = D+1
+    diag_cov = np.eye(hyp_D)*(0.2)
+    hyp = (np.random.multivariate_normal(np.zeros(hyp_D), diag_cov))
+    _test_kernel_gradient_(sqr_exp, hyp, X)
 
 def test_matern_compute_sanity_checks():
     matern = Matern(3)
@@ -46,7 +55,6 @@ def test_matern_compute_sanity_checks():
         in execinfo.value.args[0]
     )
 
-
 def test_matern_invalid_degree():
     for degree in [0, 2, 4, 6]:
         with pytest.raises(ValueError) as execinfo:
@@ -55,3 +63,112 @@ def test_matern_invalid_degree():
             "Only degrees 1, 3 and 5 are supported for the"
             in execinfo.value.args[0]
         )
+
+def test_matern_kernel_gradient():
+    matern_fun = Matern(3)
+    D = 3
+    N = 20
+    diag_cov = np.eye(N)*(0.2)
+    X = (np.random.multivariate_normal(np.zeros(N), diag_cov, D)).T
+    hyp_D = D+1
+    diag_cov = np.eye(hyp_D)*(0.2)
+    hyp = (np.random.multivariate_normal(np.zeros(hyp_D), diag_cov))
+
+    _test_kernel_gradient_(matern_fun, hyp, X)
+
+def test_rational_quad_ard_checks():
+    rq_ard = RationalQuadraticARD()
+    D = 3
+    N = 20
+    X = np.ones((N, D))
+
+    with pytest.raises(ValueError) as execinfo:
+        hyp = np.ones(D + 3)
+        rq_ard.compute(hyp, X)
+    assert (
+        "Expected 5 covariance function hyperparameters"
+        in execinfo.value.args[0]
+    )
+    with pytest.raises(ValueError) as execinfo:
+        hyp = np.ones((D + 2, 1))
+        rq_ard.compute(hyp, X)
+    assert (
+        "Covariance function output is available only for"
+        in execinfo.value.args[0]
+    )
+
+def test_cov_rational_quad_ard():
+    rq_ard = RationalQuadraticARD()
+    X = np.array([[0.343, 0.967, 0.724]]).T
+    hyp = np.array([0.5, 0.6, 0.4])
+    res = rq_ard.compute(hyp, X)
+    assert np.all(np.array([res[1, 0] - 3.3201, res[1, 0] - 3.0958, res[2, 0] - 3.2334]) < 1e-5) \
+        and np.allclose(res, res.T)
+
+def test_simple_rational_quad_ard():
+    rq_ard = RationalQuadraticARD()
+    D = 3
+    N = 20
+    X = np.ones((N, D))
+    hyp = np.ones(D + 2)
+    res = rq_ard.compute(hyp, X)
+    assert np.allclose(res[0, 0], np.array([7.389])) and np.allclose(res, res.T)
+
+def test_rqard_kernel_gradient():
+    D = 3
+    N = 20
+    diag_cov = np.eye(N)*(0.2)
+    X = (np.random.multivariate_normal(np.zeros(N), diag_cov, D)).T
+    hyp_D = D+2
+    diag_cov = np.eye(hyp_D)*(0.2)
+    hyp = (np.random.multivariate_normal(np.zeros(hyp_D), diag_cov))
+
+    rq_ard = RationalQuadraticARD()
+    _test_kernel_gradient_(rq_ard, hyp, X)
+
+def _test_kernel_gradient_(kernel_fun: AbstractKernel, hyp, X:np.ndarray, X_star:np.ndarray=None, h=1e-5, eps=1e-4):
+    """
+    Test the gradient of the kernel function via the Five-point stencil difference method (https://en.wikipedia.org/wiki/Five-point_stencil). 
+
+    Parameters
+    ----------
+    kernel_fun : AbstractKernel
+    
+    X : ndarray, shape (N, D)
+
+    hyp : ndarray, shape (cov_N,)
+        A 1D array of hyperparameters, where ``cov_N`` is
+        the number of hyperparameters.
+    h: float 
+        Grid spacing.
+    eps: float
+        Error tolerance.
+    """
+
+    K, dK = kernel_fun.compute(hyp, X, X_star,compute_grad=True)
+
+    hyp_new = hyp.copy()
+    finite_diff = np.zeros((K.shape[0], K.shape[1], len(hyp)))
+
+    for idx, h_p in enumerate(hyp.squeeze()):
+        hyp_new[idx] = h_p + 2.0 * h
+        f_2h = kernel_fun.compute(hyp_new, X, X_star)
+        hyp_new[idx] = h_p
+        
+        hyp_new[idx] = h_p + h
+        f_h = kernel_fun.compute(hyp_new, X, X_star)
+        hyp_new[idx] = h_p
+        
+        hyp_new[idx] = h_p - h
+        f_neg_h = kernel_fun.compute(hyp_new, X, X_star)
+        hyp_new[idx] = h_p
+
+        hyp_new[idx] = h_p - 2*h
+        f_neg_2h = kernel_fun.compute(hyp_new, X, X_star)
+
+        finite_diff[:, :, idx] = -f_2h + 8.0 * f_h - 8.0 * f_neg_h + f_neg_2h
+        finite_diff[:, :, idx] = finite_diff[:, :, idx] / (12 * h)
+    
+    assert np.all(np.abs(finite_diff - dK) <= eps)
+
+test_simple_rational_quad_ard()
