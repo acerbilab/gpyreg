8a84395bbdbb9a467927a0d480fd728bf9bf805e
Bobby Huggins <bobby.p.huggins@gmail.com>
2023-04-11 15:16:50 +0300

    test: add tests for new isotropic kernels.

---
+++ gpyreg/testing/test_isotropic_covariance_functions.py
@@ -0,0 +1,141 @@
+import numpy as np
+import pytest
+
+from gpyreg.covariance_functions import AbstractKernel
+from gpyreg.isotropic_covariance_functions import (
+    AbstractIsotropicKernel,
+    MaternIsotropic,
+    SquaredExponentialIsotropic,
+)
+
+
+def test_squared_exponential_isotropic_compute_sanity_checks():
+    squared_expontential = SquaredExponentialIsotropic()
+    D = 3
+    N = 20
+    X = np.ones((N, D))
+
+    with pytest.raises(ValueError) as execinfo:
+        hyp = np.ones(D + 2)
+        squared_expontential.compute(hyp, X)
+    assert (
+        "Expected 2 covariance function hyperparameters"
+        in execinfo.value.args[0]
+    )
+    with pytest.raises(ValueError) as execinfo:
+        hyp = np.ones((2, 1))
+        squared_expontential.compute(hyp, X)
+    assert (
+        "Covariance function output is available only for"
+        in execinfo.value.args[0]
+    )
+
+
+def test_sqr_exp_iso_kernel_gradient():
+    sqr_exp = SquaredExponentialIsotropic()
+    D = 3
+    N = 20
+    diag_cov = np.eye(N) * (0.2)
+    X = (np.random.multivariate_normal(np.zeros(N), diag_cov, D)).T
+    hyp_D = 2
+    diag_cov = np.eye(hyp_D) * (0.2)
+    hyp = np.random.multivariate_normal(np.zeros(hyp_D), diag_cov)
+    _test_kernel_gradient_(sqr_exp, hyp, X)
+
+
+def test_matern_isotropic_compute_sanity_checks():
+    matern = MaternIsotropic(3)
+    D = 3
+    N = 20
+    X = np.ones((N, D))
+
+    with pytest.raises(ValueError) as execinfo:
+        hyp = np.ones(D + 2)
+        matern.compute(hyp, X)
+    assert (
+        "Expected 2 covariance function hyperparameters"
+        in execinfo.value.args[0]
+    )
+    with pytest.raises(ValueError) as execinfo:
+        hyp = np.ones((2, 1))
+        matern.compute(hyp, X)
+    assert (
+        "Covariance function output is available only for"
+        in execinfo.value.args[0]
+    )
+
+
+def test_matern_isotropic_invalid_degree():
+    for degree in [0, 2, 4, 6]:
+        with pytest.raises(ValueError) as execinfo:
+            MaternIsotropic(degree)
+        assert (
+            "Only degrees 1, 3 and 5 are supported for the"
+            in execinfo.value.args[0]
+        )
+
+
+def test_matern_isotropic_kernel_gradient():
+    matern_fun = MaternIsotropic(3)
+    D = 3
+    N = 20
+    diag_cov = np.eye(N) * (0.2)
+    X = (np.random.multivariate_normal(np.zeros(N), diag_cov, D)).T
+    hyp_D = 2
+    diag_cov = np.eye(hyp_D) * (0.2)
+    hyp = np.random.multivariate_normal(np.zeros(hyp_D), diag_cov)
+
+    _test_kernel_gradient_(matern_fun, hyp, X)
+
+def _test_kernel_gradient_(
+    kernel_fun: AbstractKernel,
+    hyp,
+    X: np.ndarray,
+    X_star: np.ndarray = None,
+    h=1e-5,
+    eps=1e-4,
+):
+    """
+    Test the gradient of the kernel function via the Five-point stencil difference method (https://en.wikipedia.org/wiki/Five-point_stencil).
+
+    Parameters
+    ----------
+    kernel_fun : AbstractKernel
+
+    X : ndarray, shape (N, D)
+
+    hyp : ndarray, shape (cov_N,)
+        A 1D array of hyperparameters, where ``cov_N`` is
+        the number of hyperparameters.
+    h: float
+        Grid spacing.
+    eps: float
+        Error tolerance.
+    """
+
+    K, dK = kernel_fun.compute(hyp, X, X_star, compute_grad=True)
+
+    hyp_new = hyp.copy()
+    finite_diff = np.zeros((K.shape[0], K.shape[1], len(hyp)))
+
+    for idx, h_p in enumerate(hyp.squeeze()):
+        hyp_new[idx] = h_p + 2.0 * h
+        f_2h = kernel_fun.compute(hyp_new, X, X_star)
+        hyp_new[idx] = h_p
+
+        hyp_new[idx] = h_p + h
+        f_h = kernel_fun.compute(hyp_new, X, X_star)
+        hyp_new[idx] = h_p
+
+        hyp_new[idx] = h_p - h
+        f_neg_h = kernel_fun.compute(hyp_new, X, X_star)
+        hyp_new[idx] = h_p
+
+        hyp_new[idx] = h_p - 2 * h
+        f_neg_2h = kernel_fun.compute(hyp_new, X, X_star)
+
+        finite_diff[:, :, idx] = -f_2h + 8.0 * f_h - 8.0 * f_neg_h + f_neg_2h
+        finite_diff[:, :, idx] = finite_diff[:, :, idx] / (12 * h)
+
+    assert np.all(np.abs(finite_diff - dK) <= eps)
+
